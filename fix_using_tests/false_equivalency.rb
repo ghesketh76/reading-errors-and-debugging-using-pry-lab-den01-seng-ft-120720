028277294464aa5a17c32e8eb371032967a42fd7
Grant Hesketh <ghesketh76@gmail.com>
2020-10-30 19:11:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -10,10 +10,10 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
-def selection(get_user_input)
-  if get_user_input = 1
+def selection(num)
+  if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif get_user_input = 2
+  elsif #num = 2
      "HAM HAM HAM IN MY TUMMY"
   end
   binding.pry
